5e19a734f57c6da77cde86bfce570173da1407ac
buyagro <mariofelipesf@hotmail.com>
2025-04-06 16:52:20 -0300

    Add files via upload

--- pages/produtor.js
+++ pages/produtor.js
@@ -7,23 +7,33 @@ export default function Produtor() {
   const [volume, setVolume] = useState('');
 
   const gerarPedido = () => {
-    const msg = \`Olá, sou \${nome}, quero \${volume}L de \${produto} pelo preço negociado.\`;
-    const link = \`https://wa.me/5561999999999?text=\${encodeURIComponent(msg)}\`;
+    const msg = `Olá, sou ${nome}, quero ${volume}L de ${produto} pelo preço negociado.`;
+    const link = `https://wa.me/5561999999999?text=${encodeURIComponent(msg)}`;
     window.open(link, '_blank');
   };
 
   return (
     <Layout>
       <h2>Área do Produtor</h2>
-      <input placeholder="Seu nome" value={nome} onChange={e => setNome(e.target.value)} /><br />
+      <input
+        placeholder="Seu nome"
+        value={nome}
+        onChange={e => setNome(e.target.value)}
+      /><br />
       <select value={produto} onChange={e => setProduto(e.target.value)} style={{ marginTop: 10 }}>
         <option>Glifosato</option>
         <option>Cletodim</option>
         <option>2,4-D</option>
         <option>Fox Xpro</option>
       </select><br />
-      <input placeholder="Volume desejado (L)" value={volume} onChange={e => setVolume(e.target.value)} /><br />
-      <button style={{ marginTop: 10 }} onClick={gerarPedido}>Autorizar Compra / WhatsApp</button>
+      <input
+        placeholder="Volume desejado (L)"
+        value={volume}
+        onChange={e => setVolume(e.target.value)}
+      /><br />
+      <button style={{ marginTop: 10 }} onClick={gerarPedido}>
+        Autorizar Compra / WhatsApp
+      </button>
     </Layout>
   );
 }
